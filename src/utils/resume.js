c546ffe2bef74b478b55370d1dd43802bf70aaea
lzq04 <69559066+lzq04@users.noreply.github.com>
2022-06-17 21:10:27 +0800

    Update resume.js

--- src/utils/resume.js
+++ src/utils/resume.js
@@ -1,15 +1,15 @@
 export default {
     'basics': {
-        'position': 'Web 前端工程师',
-        'name': '郭海龙',
+        'position': 'Web 后端工程师',
+        'name': '李倾城',
         'gender': '男',
         'age': '28',
         'worklife': '3',
         'label': '我是一个小胖子，没事就摸肚子',
         'picture': 'http://www.lgstatic.com/i/image/M00/A1/9D/CgqKkVioBICAXXVyAABFXV8M2hw21.jpeg',
-        'email': '724355924@qq.com',
-        'phone': '18521708248',
-        'website': 'https://github.com/skyvow',
+        'email': '2287252931@qq.com',
+        'phone': '13598361220',
+        'website': 'https://github.com/lzq04',
         'summary': [
             '个人主攻 Web 前端和 NodeJS 开发，3年+前端开发经验，参与或独立开发过多个核心项目，项目类型涉及：电子商务、教育服务、企业管理、SPA 应用（PC/Moblie）等等',
             '喜欢关注 Web 前端相关新技术（React、Angular、Vue...），对于新技术有很强的学习兴趣',
